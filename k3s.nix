16025eb242c45570c8c8dc214e7e43133fa61d88
Jason van Eeden <jasonrve@gmail.com>
2024-08-06 19:36:18 +0200

    Update k3s.nix

--- k3s.nix
+++ k3s.nix
@@ -3,7 +3,4 @@
 {
   services.k3s.enable = true;
   services.k3s.role = "server";
-  services.k3s.extraFlags = pkgs.lib.mkForce [
-    # "--kubelet-arg=v=4" # Optionally add additional args to k3s
-  ];
 }
