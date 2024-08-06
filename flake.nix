685de90b21d179837a057572112491f1b7657af2
Jason van Eeden <jasonrve@gmail.com>
2024-08-06 19:59:45 +0200

    updated

--- flake.nix
+++ flake.nix
@@ -1,12 +1,10 @@
 {
-  description = "A NixOS configuration for k3s";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
+  outputs = { self, nixpkgs, nixos-hardware }:{
     nixosConfigurations = {
       my-hostname = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
