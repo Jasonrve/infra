0fb4e450c03d28a9c5e7de668774bb3eeadc35a6
Jason van Eeden <jasonrve@gmail.com>
2024-08-07 12:43:58 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
